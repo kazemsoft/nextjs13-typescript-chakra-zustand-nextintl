23352a96136113916846ef62ecd223146515fb2f
Kazem Qulian <kgholian@gamil.com>
2023-08-29 12:14:43 +0330

    removing not needed boilerplate

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,8 +1,8 @@
 import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
-  initialColorMode: "dark",
-  useSystemColorMode: false,
+  initialColorMode: "system",
+  useSystemColorMode: true,
 };
 
 export const theme = extendTheme({
